f9dd226a35b96e82449e31a8dae22e45c44499b1
Jamie Evans <jamieevans@itg.co.uk>
2019-05-03 16:00:04 +0100

    cards completed with information from old website

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -21,10 +21,7 @@
     <div class="intro--content">
         <h1>Professional <span>-</span> Qualified <span>-</span> Reliable</h1>
         <p>Limited Company Number 11285968</p>
-        <p>Welcome to the Wyn Edwards Electrical website, We are a family run electrical installation company based
-            in Rhyl, on the beautiful North Wales coast. We undertake a variety of work from simple socket changes
-            to full re-wires. We take great pride in our work and our business relies on our customer satisfaction.
-            If you visit our Facebook page you can see our 5 star feedback.</p>
+        <p>Welcome to the Wyn Edwards Electrical website, We are a family run electrical installation company based in Rhyl, on the beautiful North Wales coast. We undertake a variety of work from simple socket changes to full re-wires. We take great pride in our work and our business relies on our customer satisfaction. If you visit our Facebook page you can see our 5 star feedback.</p>
     </div>
 
     <div class="arrow" id="intro_arrow">
@@ -38,14 +35,25 @@
 <section class="services" id="services">
     <div class="services--content">
         <h1>Services</h1>
-       <p>Here are some of the services I offer. Please feel free to contact me for a quote, or if any service is required that's not on this list.</p>
+        <p>Here are some of the services I offer. Please feel free to contact me for a quote, or if any service is
+            required that's not on this list.</p>
+        <p>(click or hover for more information)</p>
     </div>
-    
+
     @include('includes.cards')
 
+    <div class="arrow" id="services_arrow">
+        <a href="#contact">
+            <i class="fas fa-chevron-down fa-bounce"></i>
+        </a>
+    </div>
 </section>
 {{-- END SERVICES --}}
+<section class="contact" id="contact">
+    <div class="contact--content">
 
+    </div>
+</section>
 {{-- TESTIMONIALS --}}
 <section class="testimonials">
 
