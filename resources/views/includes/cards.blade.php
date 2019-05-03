e662917a53e4d040e5a10d475a2dbd3e806265fa
Jamie Evans <taichi.chikung@gmail.com>
2019-05-03 22:21:30 +0100

    merge

--- resources/views/includes/cards.blade.php
+++ resources/views/includes/cards.blade.php
@@ -3,10 +3,10 @@
 {{-- card container --}}
 <div class="cards--container" id="cards">
 
-    <div class="card">
+    <div class="card" onclick="">
         {{-- card image --}}
         <div class="card--img">
-            <img src="{{ asset('images/sec.jpg') }}" alt="" width="100%">
+            <img src="{{ asset('images/rewire.jpg') }}" alt="" width="100%">
         </div>
         {{-- card content - will grow --}}
         <div class="card--content">
@@ -15,35 +15,31 @@
         </div>
     </div>
 
-    <div class="card">
+     <div class="card" onclick="">
         {{-- card image --}}
         <div class="card--img">
-            <img src="{{ asset('images/sec.jpg') }}" alt="" width="100%">
+            <img src="{{ asset('images/pat.png') }}" alt="" width="100%">
         </div>
         {{-- card content - will grow --}}
         <div class="card--content">
-            <div>
-                <h3>Portable Appliance Testing</h3>
-                <p>If you are looking to have your equipment PAT tested; then look no further. We have various offers that will please everyone. We also offer an "Out of Hours" service to minimise the impact on your business during social hours. Please get in contact and discuss your individual needs.</p>
-            </div>
+            <h3>Portable Appliance Testing</h3>
+            <p>If you are looking to have your equipment PAT tested; then look no further. We have various offers that will please everyone. We also offer an "Out of Hours" service to minimise the impact on your business during social hours. Please get in contact and discuss your individual needs.</p>
         </div>
     </div>
 
-    <div class="card">
+     <div class="card" onclick="">
         {{-- card image --}}
         <div class="card--img">
-            <img src="{{ asset('images/sec.jpg') }}" alt="" width="100%">
+            <img src="{{ asset('images/kitchen.jpg') }}" alt="" width="100%">
         </div>
         {{-- card content - will grow --}}
         <div class="card--content">
-            <div>
-                <h3>Kitchen Extension/Upgrade</h3>
-                <p>Getting the electrics right is an important part of kitchen design and it's  vital to get everything in the right place. While upgrading your kitchen design it   makes sense to completely over-spec the electricity supply and provisions for   appliances and gadgets as it's not a job you want to have to revisit a couple of   years down the line.</p>
-            </div>
+            <h3>Kitchen Extension/Upgrade</h3>
+            <p>Getting the electrics right is an important part of kitchen design and it's  vital to get everything in the right place. While upgrading your kitchen design it   makes sense to completely over-spec the electricity supply and provisions for   appliances and gadgets as it's not a job you want to have to revisit a couple of   years down the line.</p>
         </div>
     </div>
 
-    <div class="card">
+     <div class="card" onclick="">
         {{-- card image --}}
         <div class="card--img">
             <img src="{{ asset('images/sec.jpg') }}" alt="" width="100%">
@@ -51,45 +47,38 @@
         {{-- card content - will grow --}}
         <div class="card--content">
             <h3>Security Lighting</h3>
-            <p>Security lighting is a brilliant way to deter any unwanted intruders and it is perfect for lighting
-                up your home when you return and it's dark . Security lights use Passive Infrared (P.I.R) technology
-                to detect human presence. These units have vastly improved over the years and are no longer prone to
-                lighting up with small animal presence.</p>
+            <p>Security lighting is a brilliant way to deter any unwanted intruders and it is perfect for lighting up your home when you return and it's dark . Security lights use Passive Infrared (P.I.R) technology to detect human presence. These units have vastly improved over the years and are no longer prone to lighting up with small animal presence.</p>
         </div>
     </div>
 
-    <div class="card">
+     <div class="card" onclick="">
         {{-- card image --}}
         <div class="card--img">
-            <img src="{{ asset('images/sec.jpg') }}" alt="" width="100%">
+            <img src="{{ asset('images/fault.jpg') }}" alt="" width="100%">
         </div>
         {{-- card content - will grow --}}
         <div class="card--content">
-            <div>
-                <h3>Fault Finding</h3>
-                <p>Locating a fault in an electrical system quickly   requires  experience. We are trained and experienced at electrical fault finding and repairing domestic electric installations. This experience ensures a fast, efficient and cost-effective service. Once the fault has been identified, it can either be repaired immediately or booked to be carried out at a time convenient to you.</p>
-            </div>
+            <h3>Fault Finding</h3>
+            <p>Locating a fault in an electrical system quickly   requires  experience. We are trained and experienced at electrical fault finding and repairing domestic electric installations. This experience ensures a fast, efficient and cost-effective service. Once the fault has been identified, it can either be repaired immediately or booked to be carried out at a time convenient to you.</p>
         </div>
     </div>
 
-    <div class="card">
+     <div class="card" onclick="">
         {{-- card image --}}
         <div class="card--img">
-            <img src="{{ asset('images/sec.jpg') }}" alt="" width="100%">
+            <img src="{{ asset('images/garage.jpg') }}" alt="" width="100%">
         </div>
         {{-- card content - will grow --}}
         <div class="card--content">
-            <div>
-                <h3>Outdoor Electric & Garages</h3>
-                <p>From a simple outdoor socket to a fully fitted out garage; we have it covered! We can set up power supplies to your water feature or put a socket at the bottom of the garden, you ask - we deliver. All of our outdoor work is protected by an RCD, (Residual Current Device) designed to trip the circuit in the circumstance that you come into contact with an exposed live part or a cable become damadged.</p>
-            </div>
+            <h3>Outdoor Electric & Garages</h3>
+            <p>From a simple outdoor socket to a fully fitted out garage; we have it covered! We can set up power supplies to your water feature or put a socket at the bottom of the garden, you ask - we deliver. All of our outdoor work is protected by an RCD, (Residual Current Device) designed to trip the circuit in the circumstance that you come into contact with an exposed live part or a cable become damadged.</p>
         </div>
     </div>
 
-    <div class="card">
+    <div class="card" onclick="">
         {{-- card image --}}
         <div class="card--img">
-            <img src="{{ asset('images/sec.jpg') }}" alt="" width="100%">
+            <img src="{{ asset('images/water.jpg') }}" alt="" width="100%">
         </div>
         {{-- card content - will grow --}}
         <div class="card--content">
@@ -98,33 +87,30 @@
         </div>
     </div>
 
-    <div class="card">
+    <div class="card" onclick="">
         {{-- card image --}}
         <div class="card--img">
-            <img src="{{ asset('images/sec.jpg') }}" alt="" width="100%">
+            <img src="{{ asset('images/outlet.jpg') }}" alt="" width="100%">
         </div>
         {{-- card content - will grow --}}
         <div class="card--content">
-            <div>
-                <h3>Caravan Power Outlets</h3>
-                <p>When you connect to a campsite electric hook-up point you are able to receive an electricity supply as you would at home. This will be a nominal 230V, single phase, 50Hz supply, which is compatible with all UK and modern European caravans and motorhomes.</p>
-            </div>
+            <h3>Caravan Power Outlets</h3>
+            <p>When you connect to a campsite electric hook-up point you are able to receive an electricity supply as you would at home. This will be a nominal 230V, single phase, 50Hz supply, which is compatible with all UK and modern European caravans and motorhomes.</p>
         </div>
     </div>
 
-    <div class="card">
+    <div class="card" onclick="">
         {{-- card image --}}
         <div class="card--img">
-            <img src="{{ asset('images/sec.jpg') }}" alt="" width="100%">
+            <img src="{{ asset('images/van.jpg') }}" alt="" width="100%">
         </div>
         {{-- card content - will grow --}}
         <div class="card--content">
-            <div>
-                <h3>Emergency Call-Out</h3>
-                <p>If you have an Electrical Emergency; we can help! Here at Wyn Edwards Electrical, we have a 24 hour call-out service. Simply call the Emergency contact number on the contact page.</p>
-            </div>
+            <h3>Emergency Call-Out</h3>
+            <p>If you have an Electrical Emergency; we can help! Here at Wyn Edwards Electrical, we have a 24 hour call-out service. Simply call the Emergency contact number on the contact page.</p>
         </div>
     </div>
+   
 </div>
 {{-- end cards container --}}
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -19,16 +19,16 @@
 
 <section class="intro" id="intro">
     <div class="intro--content">
-        <h1>Professional - Qualified - Reliable</h1>
+        <h1>Professional <span>-</span> Qualified <span>-</span> Reliable</h1>
         <p>Limited Company Number 11285968</p>
-        <p>Welcome to the Wyn Edwards Electrical website, We are a family run electrical installation company based
-            in Rhyl, on the beautiful North Wales coast. We undertake a variety of work from simple socket changes
-            to full re-wires. We take great pride in our work and our business relies on our customer satisfaction.
-            If you visit our Facebook page you can see our 5 star feedback.</p>
+        <p>Welcome to the Wyn Edwards Electrical website, We are a family run electrical installation company based in
+            Rhyl, on the beautiful North Wales coast. We undertake a variety of work from simple socket changes to full
+            re-wires. We take great pride in our work and our business relies on our customer satisfaction. If you visit
+            our Facebook page you can see our 5 star feedback.</p>
     </div>
 
     <div class="arrow" id="intro_arrow">
-        <a href="#cards">
+        <a href="#services">
             <i class="fas fa-chevron-down fa-bounce"></i>
         </a>
     </div>
@@ -36,14 +36,58 @@
 
 {{-- SERVICES-CONTAINER --}}
 <section class="services" id="services">
+    <div class="services--content">
+        <h1>Services</h1>
+        <p>Here are some of the services I offer. Please feel free to contact me for a quote, or if any service is
+            required that's not on this list.</p>
+        <p>(click or hover for more information)</p>
+    </div>
 
     @include('includes.cards')
 
+    <div class="arrow" id="services_arrow">
+        <a href="#contact">
+            <i class="fas fa-chevron-down fa-bounce"></i>
+        </a>
+    </div>
 </section>
 {{-- END SERVICES --}}
 
+{{-- CONTACT SECTION --}}
+<section class="contact" id="contact">
+    <div class="contact--content">
+        <h1>Contact me</h1>
+        <p>Please feel free to fill in the form below and get in touch.</p>
+        <p> Wether it's for a quote, some feedback or any questions you many about electronics or my services.<br> I will
+            respond as soon as possible. You can also contact me on facebook.</p>
+
+        <form action="POST" method="">
+            @csrf
+            <div class="contact--form">
+                <div class="contact--inputs">
+                    <input type="text" name="name" id="name" placeholder="Name" required>
+                    <input type="email" name="email" id="email" placeholder="Email Address" required>
+                    <input type="text" name="location" id="location" placeholder="Location">
+                </div>
+
+                <div class="contact--query">
+                    <textarea name="query" id="query" cols="30" rows="9" placeholder="Query"></textarea>
+                </div>
+            </div>
+
+            <button type="submit">Submit</button>
+
+        </form>
+    </div>
+
+    <div class="arrow" id="contact_arrow">
+        <a href="#services">
+            <i class="fas fa-chevron-down fa-bounce"></i>
+        </a>
+    </div>
+</section>
 {{-- TESTIMONIALS --}}
-<section class="testimonials">
+<section class="testimonials" id="testimonials">
 
 </section>
 {{-- END TESTIMONIALS --}}
