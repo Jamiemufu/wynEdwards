1a744f4c7199aa111c58db307f946a27eef4a30c
Jamie Evans <taichi.chikung@gmail.com>
2019-05-03 02:23:30 +0100

    Starting on this again

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,24 +8,46 @@
         <div class="main-logo">
             <img src="images/wynpng.png" alt="" width="100%">
         </div>
-        <div class="arrow" id="arrow">
-            <a href="#services">
+
+        <div class="arrow" id="splash_arrow">
+            <a href="#intro">
                 <i class="fas fa-chevron-down fa-bounce"></i>
             </a>
         </div>
     </div>
 </section>
-{{-- END MAIN --}}
+
+<section class="intro" id="intro">
+    <div class="intro--content">
+        <h1>Professional - Qualified - Reliable</h1>
+        <p>Limited Company Number 11285968</p>
+        <p>Welcome to the Wyn Edwards Electrical website, We are a family run electrical installation company based
+            in Rhyl, on the beautiful North Wales coast. We undertake a variety of work from simple socket changes
+            to full re-wires. We take great pride in our work and our business relies on our customer satisfaction.
+            If you visit our Facebook page you can see our 5 star feedback.</p>
+    </div>
+
+    <div class="arrow" id="intro_arrow">
+        <a href="#cards">
+            <i class="fas fa-chevron-down fa-bounce"></i>
+        </a>
+    </div>
+</section>
+
 {{-- SERVICES-CONTAINER --}}
 <section class="services" id="services">
 
+    @include('includes.cards')
+
 </section>
 {{-- END SERVICES --}}
+
 {{-- TESTIMONIALS --}}
 <section class="testimonials">
 
 </section>
 {{-- END TESTIMONIALS --}}
+{{-- END MAIN --}}
 
 
 
